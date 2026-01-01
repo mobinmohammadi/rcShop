a21426ec0f1a6e7adc27affceab68181d5dc1682
mobinJS <mobyn3223>
2026-01-02 02:09:44 +0330

    updated

--- src/Page/Product/Page.tsx
+++ src/Page/Product/Page.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import RightContent from '../../Components/Product/RightContent/RightContent'
 import LeftContent from '../../Components/Product/LeftContent/LeftContent'
 import Header from '../../Components/Header/Header'
